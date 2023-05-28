0a3671906e5948f0b4401b7feb4fc3b7a0b2f3cf
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-05-28 19:54:31 +0300

    Update SearchInput.tsx

--- src/components/ui/SearchInput.tsx
+++ src/components/ui/SearchInput.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { Search } from "lucide-react";
 
 interface SearchInputProps {
   onSubmit: (data: string) => void;
@@ -17,20 +17,11 @@ const SearchInput: React.FC<SearchInputProps> = ({
   onSubmit,
   onChange,
 }) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+
+  const [search, setSearch] = useState("");
 
   return (
-    <form
-      onSubmit={
-        void handleSubmit((data) =>
-          onSubmit((data as { search: string }).search)
-        )
-      }
-    >
+    <>
       <label
         htmlFor="default-search"
         className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -39,39 +30,31 @@ const SearchInput: React.FC<SearchInputProps> = ({
       </label>
       <div className="relative">
         <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <svg
-            aria-hidden="true"
-            className="h-5 w-5 text-gray-500 dark:text-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
+          <Search className="h-5 w-5 text-gray-500 dark:text-gray-400" />
         </div>
         <input
-          {...register("search", { minLength: 3 })}
           type="search"
           id="default-search"
           className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
           placeholder={placeholder ?? "Поиск"}
-          onChange={(e) => onChange?.(e.target.value)}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            onChange?.(e.target.value);
+          }}
           required
         />
         <button
           type="submit"
           className="absolute bottom-2.5 right-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
+          onClick={(e) => {
+            e.preventDefault();
+            onSubmit(search);
+          }}
         >
           {button ?? "Найти"}
         </button>
       </div>
-    </form>
+    </>
   );
 };
 
