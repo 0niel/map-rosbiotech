b3df30068f9506c55f864dfde2575eec358c33f9
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-08-22 02:10:46 +0300

    Fix page viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -46,6 +46,8 @@ const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { s
         <meta property="og:site_name" content="Карта РТУ МИРЭА" />
         <meta property="og:url" content="https://map.mirea.ru" />
         <meta property="og:image" content="https://map.mirea.ru/icons/apple-touch-icon.png" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
       </Head>
       <main>
         <QueryClientProvider client={queryClient}>
