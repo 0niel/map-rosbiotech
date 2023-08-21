4adabe6aa4fd583261eaa00d60276961b9f6d0b3
Dragonprod <artyom2002-98@mail.ru>
2023-08-21 10:42:39 +0300

    fix: disable search and additional features

--- src/components/ui/HeaderNavbar.tsx
+++ src/components/ui/HeaderNavbar.tsx
@@ -73,13 +73,13 @@ const HeaderNavbar = () => {
                 <Image src="mirea-gerb.svg" className="mr-3 h-8" alt="РТУ МИРЭА Герб" width={32} height={32} />
                 <span className="hidden self-center whitespace-nowrap text-2xl font-semibold sm:block">РТУ МИРЭА</span>
               </div>
-              <div className="hidden lg:block lg:pl-2">
-                <SearchButton
-                  onClick={function (): void {
-                    throw new Error("Function not implemented.")
-                  }}
-                />
-              </div>
+              {/*<div className="hidden lg:block lg:pl-2">*/}
+              {/*  <SearchButton*/}
+              {/*    onClick={function (): void {*/}
+              {/*      throw new Error("Function not implemented.")*/}
+              {/*    }}*/}
+              {/*  />*/}
+              {/*</div>*/}
             </div>
             <div className="flex items-center lg:order-2">
               <button
@@ -91,47 +91,47 @@ const HeaderNavbar = () => {
               </button>
 
               {/* <!-- Apps --> */}
-              <Dropdown
-                label=""
-                renderTrigger={(_) => (
-                  <button
-                    type="button"
-                    className="rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 focus:ring-4 focus:ring-gray-300"
-                  >
-                    <span className="sr-only">Дополнительные возможности</span>
-                    <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
-                    </svg>
-                  </button>
-                )}
-              >
-                <div className="my-4 max-w-sm list-none divide-y divide-gray-100 overflow-hidden rounded text-base">
-                  <div className="block px-4 py-2 text-center text-base font-medium text-gray-700  ">Дополнительно</div>
-                  <div className="grid grid-cols-3 gap-4 p-4">
-                    <a
-                      href="#"
-                      className="group block rounded-lg p-4 text-center hover:bg-gray-100 dark:hover:bg-gray-600"
-                    >
-                      <HiMap className="mx-auto mb-1 h-7 w-7 text-gray-400 group-hover:text-gray-500 " />
-                      <div className="text-sm text-gray-900 dark:text-white">тепловая карта</div>
-                    </a>
-                    <a
-                      href="#"
-                      className="group block rounded-lg p-4 text-center hover:bg-gray-100 dark:hover:bg-gray-600"
-                    >
-                      <FaRegCalendarAlt className="mx-auto mb-1 h-7 w-7 text-gray-400 group-hover:text-gray-500 " />
-                      <div className="text-sm text-gray-900 dark:text-white">свободные аудитории</div>
-                    </a>
-                    <a
-                      href="#"
-                      className="group block rounded-lg p-4 text-center hover:bg-gray-100 dark:hover:bg-gray-600"
-                    >
-                      <BiTimeFive className="mx-auto mb-1 h-7 w-7 text-gray-400 group-hover:text-gray-500 " />
-                      <div className="text-sm text-gray-900 dark:text-white">дата и время</div>
-                    </a>
-                  </div>
-                </div>
-              </Dropdown>
+              {/*<Dropdown*/}
+              {/*  label=""*/}
+              {/*  renderTrigger={(_) => (*/}
+              {/*    <button*/}
+              {/*      type="button"*/}
+              {/*      className="rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 focus:ring-4 focus:ring-gray-300"*/}
+              {/*    >*/}
+              {/*      <span className="sr-only">Дополнительные возможности</span>*/}
+              {/*      <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">*/}
+              {/*        <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>*/}
+              {/*      </svg>*/}
+              {/*    </button>*/}
+              {/*  )}*/}
+              {/*>*/}
+              {/*  <div className="my-4 max-w-sm list-none divide-y divide-gray-100 overflow-hidden rounded text-base">*/}
+              {/*    <div className="block px-4 py-2 text-center text-base font-medium text-gray-700  ">Дополнительно</div>*/}
+              {/*    <div className="grid grid-cols-3 gap-4 p-4">*/}
+              {/*      <a*/}
+              {/*        href="#"*/}
+              {/*        className="group block rounded-lg p-4 text-center hover:bg-gray-100 dark:hover:bg-gray-600"*/}
+              {/*      >*/}
+              {/*        <HiMap className="mx-auto mb-1 h-7 w-7 text-gray-400 group-hover:text-gray-500 " />*/}
+              {/*        <div className="text-sm text-gray-900 dark:text-white">тепловая карта</div>*/}
+              {/*      </a>*/}
+              {/*      <a*/}
+              {/*        href="#"*/}
+              {/*        className="group block rounded-lg p-4 text-center hover:bg-gray-100 dark:hover:bg-gray-600"*/}
+              {/*      >*/}
+              {/*        <FaRegCalendarAlt className="mx-auto mb-1 h-7 w-7 text-gray-400 group-hover:text-gray-500 " />*/}
+              {/*        <div className="text-sm text-gray-900 dark:text-white">свободные аудитории</div>*/}
+              {/*      </a>*/}
+              {/*      <a*/}
+              {/*        href="#"*/}
+              {/*        className="group block rounded-lg p-4 text-center hover:bg-gray-100 dark:hover:bg-gray-600"*/}
+              {/*      >*/}
+              {/*        <BiTimeFive className="mx-auto mb-1 h-7 w-7 text-gray-400 group-hover:text-gray-500 " />*/}
+              {/*        <div className="text-sm text-gray-900 dark:text-white">дата и время</div>*/}
+              {/*      </a>*/}
+              {/*    </div>*/}
+              {/*  </div>*/}
+              {/*</Dropdown>*/}
 
               <div className="ml-2">
                 <DropdownRadio
